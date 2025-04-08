ff6027867239886a599b1b5700bf9700aada4a8e
voltaney <voltaney61@gmail.com>
2025-04-08 20:27:45 +0900

    initial commit

---
+++ PackageTypeCheck/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace PackageTypeCheck;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
