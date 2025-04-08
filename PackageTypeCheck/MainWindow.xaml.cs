305e394ed18e1852d4005043d87e0c0865d74098
voltaney <voltaney61@gmail.com>
2025-04-08 23:19:27 +0900

    Add conditional text display for packaged/unpackaged apps
    - Added x:Name="MyText" to TextBlock in MainWindow.xaml for access in code.
    - Implemented conditional compilation in MainWindow.xaml.cs to set MyText based on app packaging.
    - Defined PACKAGED constant in PackageTypeCheck.csproj for build configuration.

--- PackageTypeCheck/MainWindow.xaml.cs
+++ PackageTypeCheck/MainWindow.xaml.cs
@@ -7,5 +7,12 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+#if PACKAGED
+        // Packaged app
+        MyText.Text = "Packaged App";
+#else
+        // Unpackaged app
+        MyText.Text = "Unpackaged App";
+#endif
     }
 }
